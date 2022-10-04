1995a8d65776d9ee3472eae8f07eb4cc31759293
Kaylie Phillips <kaylie.travel@gmail.com>
2022-10-04 10:56:03 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            word = word.ToLower();
+            var reverseWord = string.Join("", word.ToLower().Reverse());
+            
+            if (word == reverseWord)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,28 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("Hannah", true)]
+        [InlineData("wow",true)]
+        [InlineData("house", false)]
+        [InlineData("street car", false)]
+        
+        public void Test1(string word, bool expected)
         {
+            //arrange
+            var wordTest1 = new WordSmith(); 
+
+            //act
+            var actual = wordTest1.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+
+
+
 
         }
     }
